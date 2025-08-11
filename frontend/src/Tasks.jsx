8f2a55a29b812119f55b58d0381ac08c0acd6d87
msllagas <mndyllagas@gmail.com>
2025-08-11 20:25:56 +0800

    feat(task): implement create functionality

--- frontend/src/Tasks.jsx
+++ frontend/src/Tasks.jsx
@@ -3,6 +3,7 @@ import api from "./api/axios"
 
 function Tasks() {
   const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
 
   useEffect(() => {
     api.get('/tasks')
@@ -10,9 +11,42 @@ function Tasks() {
       .catch(err => console.error(err));
   }, []);
 
+
+  const createTask = async (event) => {
+    event.preventDefault();
+
+    if (!newTask.trim()) return;
+
+    try {
+      const response = await api.post('/tasks', {
+        title: newTask,
+        isDone: false,
+      });
+
+      setTasks([...tasks, response.data]);
+      setNewTask('');
+    } catch (error) {
+      alert('Failed to create task');
+    }
+  }
+
   return (
     <div>
       <h2>Tasks</h2>
+
+      <form onClick={createTask}>
+        <input
+            type="text"
+            placeholder="Enter new task"
+            value={newTask}
+            onChange={e => setNewTask(e.target.value)}
+        />
+        <button type="submit">Add Task</button>
+      </form>
+
+       {tasks.length === 0 ? (
+      <p>No tasks found.</p>
+    ) : (
       <ul>
         {tasks.map(task => (
           <li key={task.id}>
@@ -20,6 +54,7 @@ function Tasks() {
           </li>
         ))}
       </ul>
+    )}
     </div>
   );
 }
